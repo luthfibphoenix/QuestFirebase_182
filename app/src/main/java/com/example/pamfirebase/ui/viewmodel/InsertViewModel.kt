80cfa33e1cbdc28fb2ffe5cacde730d6d6374014
luthfibphoenix <lutfibahtiar2409@gmail.com>
2025-01-11 19:55:33 +0700

    membuat data class FormErrorState

--- app/src/main/java/com/example/pamfirebase/ui/viewmodel/InsertViewModel.kt
+++ app/src/main/java/com/example/pamfirebase/ui/viewmodel/InsertViewModel.kt
@@ -3,6 +3,19 @@ package com.example.pamfirebase.ui.viewmodel
 import com.example.pamfirebase.Model.Mahasiswa
 
 
+data class FormErrorState(
+    val nim: String? = null,
+    val nama: String? = null,
+    val jenis_kelamin: String? = null,
+    val alamat: String? = null,
+    val kelas: String? = null,
+    val angkatan: String? = null
+){
+    fun isValid(): Boolean{
+        return nim != null && nama != null && jenis_kelamin != null && alamat != null && kelas != null && angkatan != null
+    }
+}
+
 data class MahasiswaEvent (
     val nim: String = "",
     val nama: String = "",
