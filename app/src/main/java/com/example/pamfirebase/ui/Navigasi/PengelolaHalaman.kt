70ab36821d24d11c9b30e40de44bbe97ce2fedc8
luthfibphoenix <lutfibahtiar2409@gmail.com>
2025-01-11 20:36:37 +0700

    membuat file PengelolaHalaman dan menyelesaikan code

---
+++ app/src/main/java/com/example/pamfirebase/ui/Navigasi/PengelolaHalaman.kt
@@ -0,0 +1,41 @@
+package com.example.pamfirebase.ui.Navigasi
+
+import androidx.compose.runtime.Composable
+import androidx.compose.ui.Modifier
+import androidx.navigation.NavHostController
+import androidx.navigation.compose.NavHost
+import androidx.navigation.compose.composable
+import androidx.navigation.compose.rememberNavController
+import com.example.pamfirebase.ui.view.HomeView
+import com.example.pamfirebase.ui.view.InsertMhsView
+
+
+@Composable
+fun PengelolaHalaman(
+    modifier: Modifier,
+    navController: NavHostController = rememberNavController()
+){
+    NavHost(
+        navController = navController,
+        startDestination = DestinasiHome.route,
+        modifier = modifier
+    ){
+        composable(DestinasiHome.route){
+            HomeView(
+                navigateToItemEntry = {
+                    navController.navigate(DestinasiInsert.route)
+                },
+            )
+        }
+        composable(DestinasiInsert.route){
+            InsertMhsView(
+                onBack = {
+                    navController.popBackStack()
+                },
+                onNavigate = {
+                    navController.navigate(DestinasiHome.route)
+                }
+            )
+        }
+    }
+}
