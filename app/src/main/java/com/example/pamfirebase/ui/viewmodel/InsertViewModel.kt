b23ec9dd215ec844c0787a139d76e33d8f59443c
luthfibphoenix <lutfibahtiar2409@gmail.com>
2025-01-11 19:53:55 +0700

    membuat file InsertViewModel dan membuat fungsi MahasiswaEvent dan data class nya

---
+++ app/src/main/java/com/example/pamfirebase/ui/viewmodel/InsertViewModel.kt
@@ -0,0 +1,22 @@
+package com.example.pamfirebase.ui.viewmodel
+
+import com.example.pamfirebase.Model.Mahasiswa
+
+
+data class MahasiswaEvent (
+    val nim: String = "",
+    val nama: String = "",
+    val jenis_kelamin: String = "",
+    val alamat: String = "",
+    val kelas: String = "",
+    val angkatan: String = ""
+)
+
+fun MahasiswaEvent.toMhsModel(): Mahasiswa = Mahasiswa(
+    nim = nim,
+    nama = nama,
+    jenis_kelamin = jenis_kelamin,
+    alamat = alamat,
+    kelas = kelas,
+    angkatan = angkatan
+)
