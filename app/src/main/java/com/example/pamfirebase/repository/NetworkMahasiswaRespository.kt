ffe1121bc09afebdb9e891d2eaa9ce6440fb55f5
luthfibphoenix <lutfibahtiar2409@gmail.com>
2025-01-11 19:35:48 +0700

    melengkapi fungsi deleteMahasiswa

--- app/src/main/java/com/example/pamfirebase/repository/NetworkMahasiswaRespository.kt
+++ app/src/main/java/com/example/pamfirebase/repository/NetworkMahasiswaRespository.kt
@@ -55,9 +55,14 @@ class NetworkMahasiswaRespository (
         TODO("Not yet implemented")
     }
 
-    override suspend fun deleteMahasiswa(nim: String) {
-        TODO("Not yet implemented")
+    override suspend fun deleteMahasiswa(mahasiswa: Mahasiswa) {
+        try {
+            firestore.collection("Mahasiswa")
+                .document(mahasiswa.nim)
+                .delete()
+                .await()
+        } catch (e: Exception) {
+            throw Exception("Gagal menghapus data mahasiswa: ${e.message}")
+        }
     }
-
-
 }
